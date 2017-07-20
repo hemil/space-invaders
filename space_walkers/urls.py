23480f9efe805acce18df3de2fb3e387676e338e
hemil <hemil.shah@grofers.com>
2017-07-20 14:54:50 +0530

    add main index redirect to space-invaders

--- space_walkers/urls.py
+++ space_walkers/urls.py
@@ -1,7 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
     url(r'^space-invaders/', include('space_invaders.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^.*$', RedirectView.as_view(url='space-invaders/', permanent=False), name='main_index'),
 ]
